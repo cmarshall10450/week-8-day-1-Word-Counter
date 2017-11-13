a3655aac343239fd1dab6b6601f8e313837157b6
Chris Marshall <cmarshall10450@gmail.com>
2017-11-13 16:55:14 +0000

    Can get occurrences of words.

--- app/src/main/java/com/cmarshall10450/wordcounter/WordCounter.java
+++ app/src/main/java/com/cmarshall10450/wordcounter/WordCounter.java
@@ -1,11 +1,29 @@
 package com.cmarshall10450.wordcounter;
 
 
+import java.util.HashMap;
+
 public class WordCounter {
 
   public static int countWords(String text) {
     String[] words = text.split(" ");
     return words.length;
+  }
+
+  public static HashMap<String, Integer> getWordOccurences(String text) {
+    HashMap<String, Integer> wordOccurrences = new HashMap<>();
+
+    String[] words = text.toLowerCase().split(" ");
+    for (String word : words) {
+      if (wordOccurrences.containsKey(word)) {
+        int newValue = wordOccurrences.get(word) + 1;
+        wordOccurrences.put(word, newValue);
+      } else {
+        wordOccurrences.put(word, 1);
+      }
+    }
+
+    return wordOccurrences;
   }
 
 }
--- app/src/test/java/com/cmarshall10450/wordcounter/WordCounterTest.java
+++ app/src/test/java/com/cmarshall10450/wordcounter/WordCounterTest.java
@@ -3,6 +3,8 @@ package com.cmarshall10450.wordcounter;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+
 import static org.junit.Assert.assertEquals;
 
 public class WordCounterTest {
@@ -11,6 +13,16 @@ public class WordCounterTest {
   public void testCanCountWords() {
     int wordCount = WordCounter.countWords("Hello World");
     assertEquals(2, wordCount);
+  }
+
+  @Test
+  public void testCanGetWordOccurrences() {
+    String text = "It it it was was was";
+    HashMap<String, Integer> words = new HashMap<>();
+    words.put("it", 3);
+    words.put("was", 3);
+
+    assertEquals(words.entrySet(), WordCounter.getWordOccurences(text).entrySet());
   }
 
 }
